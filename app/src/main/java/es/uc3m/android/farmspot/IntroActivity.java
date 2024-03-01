616be12add405be3d1f0e28fd27ac408e255f32a
Daniel Kwapien <danikwapien@gmail.com>
2024-03-01 14:48:54 +0100

    First login view

---
+++ app/src/main/java/es/uc3m/android/farmspot/IntroActivity.java
@@ -0,0 +1,14 @@
+package es.uc3m.android.farmspot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
