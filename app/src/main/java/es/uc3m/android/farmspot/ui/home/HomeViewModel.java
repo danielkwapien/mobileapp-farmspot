348016d5577735a5717bc0c63420b00fc9b91014
Daniel Kwapien <danikwapien@gmail.com>
2024-03-01 13:42:12 +0100

    Initial commit

---
+++ app/src/main/java/es/uc3m/android/farmspot/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package es.uc3m.android.farmspot.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/es/uc3m/android/farmspot/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,19 @@
+package es.uc3m.android.farmspot.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
