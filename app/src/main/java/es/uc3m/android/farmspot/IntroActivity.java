f9ac49b03bc9ab4a24eabb27a28521eb12c3ea07
Daniel Kwapien <danikwapien@gmail.com>
2024-03-01 20:57:15 +0100

    adjustements to intro page appereance

--- app/src/main/java/es/uc3m/android/farmspot/IntroActivity.java
+++ app/src/main/java/es/uc3m/android/farmspot/IntroActivity.java
@@ -3,14 +3,23 @@ package es.uc3m.android.farmspot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.Objects;
+
 public class IntroActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
         setContentView(R.layout.activity_intro);
     }
 }
