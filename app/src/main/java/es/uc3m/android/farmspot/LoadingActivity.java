db8de759d9186c81c2a5c03008ccc1deb25228ac
Daniel Kwapien <danikwapien@gmail.com>
2024-03-16 14:12:58 +0100

    create loading activity

---
+++ app/src/main/java/es/uc3m/android/farmspot/LoadingActivity.java
@@ -0,0 +1,14 @@
+package es.uc3m.android.farmspot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
