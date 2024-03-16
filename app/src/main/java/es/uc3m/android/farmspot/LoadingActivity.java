2b550ebd24768f5f5d0481ac54fa368dad4d0b9d
Daniel Kwapien <danikwapien@gmail.com>
2024-03-16 14:27:32 +0100

    loading screen activity

--- app/src/main/java/es/uc3m/android/farmspot/LoadingActivity.java
+++ app/src/main/java/es/uc3m/android/farmspot/LoadingActivity.java
@@ -2,13 +2,41 @@ package es.uc3m.android.farmspot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
 
 public class LoadingActivity extends AppCompatActivity {
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
         setContentView(R.layout.activity_loading);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                openIntroActivity();
+            }
+        }, 2000);
+
+
+    }
+
+    public void openIntroActivity(){
+        Intent intent = new Intent(this, IntroActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
