fddaad84ef939d64b95ae9024ac79b9d622857ca
Daniel Kwapien <danikwapien@gmail.com>
2024-05-05 22:21:14 +0200

    add the same to signup

--- app/src/main/java/es/uc3m/android/farmspot/LoginActivity.java
+++ app/src/main/java/es/uc3m/android/farmspot/LoginActivity.java
@@ -75,6 +75,9 @@ public class LoginActivity extends AppCompatActivity {
                             editor.putBoolean("isLoggedIn", true);
                             editor.apply();
 
+                            Toast.makeText(LoginActivity.this, "Log In successful.",
+                                    Toast.LENGTH_SHORT).show();
+
                             // Proceed to main activity
                             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(intent);
--- app/src/main/java/es/uc3m/android/farmspot/SignupActivity.java
+++ app/src/main/java/es/uc3m/android/farmspot/SignupActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -60,7 +61,30 @@ public class SignupActivity extends AppCompatActivity {
             FirebaseAuth mAuth = FirebaseAuth.getInstance();
             // Initialize Firebase Auth
             mAuth.createUserWithEmailAndPassword(userEmail, userPassword)
-                    .addOnCompleteListener(this, new AuthListener(this, mAuth));
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Save login status
+                                SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putBoolean("isLoggedIn", true);
+                                editor.apply();
+
+                                Toast.makeText(SignupActivity.this, "User created!",
+                                        Toast.LENGTH_SHORT).show();
+
+                                // Proceed to main activity
+                                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish(); // Finish the login activity
+                            } else {
+                                // Handle authentication failure
+                                Toast.makeText(SignupActivity.this, "User creation failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
         }
     }
 
