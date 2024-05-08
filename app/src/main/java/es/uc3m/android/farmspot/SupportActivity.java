2a052ee37522b660fb51b5c812fed3c10dbe993a
Diego04062003 <133352439+Diego04062003@users.noreply.github.com>
2024-05-08 14:20:14 +0200

    add navigation bar SupportActivity.java

--- app/src/main/java/es/uc3m/android/farmspot/SupportActivity.java
+++ app/src/main/java/es/uc3m/android/farmspot/SupportActivity.java
@@ -1,16 +1,19 @@
 package es.uc3m.android.farmspot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Objects;
 
 public class SupportActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,40 @@ public class SupportActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         setContentView(R.layout.activity_support);
+
+        // Initialize the bottom navigation view
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+
+        // Set listener for bottom navigation view item selection
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Handle navigation item selection
+                if (item.getItemId() == R.id.navigation_home){
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                    return true;
+                }
+                else if (item.getItemId() == R.id.navigation_search){
+                    startActivity(new Intent(getApplicationContext(), SearchActivity.class));
+                    finish();
+                    return true;
+                }
+                else if (item.getItemId() == R.id.navigation_add){
+                    return true;
+                }
+                else if (item.getItemId() == R.id.navigation_favorites){
+                    startActivity(new Intent(getApplicationContext(), FavoritesActivity.class));
+                    finish();
+                    return true;
+                }
+                else if (item.getItemId() == R.id.navigation_profile){
+                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
