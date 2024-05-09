2d851385a05bdb714ef7ebdd0a7eba91937c8601
Daniel Kwapien <danikwapien@gmail.com>
2024-05-09 23:33:36 +0200

    final version

--- app/src/main/java/es/uc3m/android/farmspot/SignupActivity.java
+++ app/src/main/java/es/uc3m/android/farmspot/SignupActivity.java
@@ -2,6 +2,7 @@ package es.uc3m.android.farmspot;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -15,12 +16,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class SignupActivity extends AppCompatActivity {
